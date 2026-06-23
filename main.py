be83ed0eccc9245f32498e53f5c2b20dd89fb333
Szymon Wawrzyczek <wawrzyczekszymon@gmail.com>
2026-06-23 18:55:08 +0200

    add bricks concept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from objects import Paddle
+from objects import Paddle, Brick
 
 pygame.init()
 
@@ -16,8 +16,17 @@ START_PADDLE = pygame.Rect(
 )
 PADDLE_VELOCITY = 10
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+BLACK: tuple[int, int, int] = (0, 0, 0)
+RED: tuple[int, int, int] = (255, 0, 0)
+
+BRICK_WIDTH: int = 150
+BRICK_HEIGHT: int = 60
+COLS: int = 8 
+ROWS: int = 1
+MARGIN: int = 8
+START_X: int = 13
+bricks: list[Brick] = []
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
@@ -25,9 +34,16 @@ running = True
 
 paddle = Paddle(START_PADDLE, WHITE, PADDLE_VELOCITY)
 
+for row in range(ROWS):
+    for col in range(COLS):
+        x = START_X + col * (BRICK_WIDTH + MARGIN)
+        bricks.append(Brick(rect=pygame.Rect(x, 10, BRICK_WIDTH, BRICK_HEIGHT), color=RED))
+
 def game():
     screen.fill(BLACK)
     paddle.draw(screen=screen)
+    for brick in bricks:
+        brick.draw(screen=screen)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] and paddle.rect.left >= 0:
